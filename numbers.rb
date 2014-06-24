4cec8e8ff91229b910e0549200761289b89dbce4
Natalie Gregory <gregorynr@gmail.com>
2014-06-24 15:57:24 -0600

    numbers.rb

--- numbers.rb
+++ numbers.rb
@@ -1,1 +1,17 @@
 numbers = [20, 33, 54, 21, 87, 32, 99, 10]
+
+#print the sum of all the elements in the array numbers
+puts numbers.inject{|sum,x| sum+x}
+
+#print the largest number in the array "numbers"
+puts numbers.max
+
+#the total number of elements in the array named 'numbers'
+puts numbers.length
+
+#print the elements of the array.
+numbers.each do |number|
+  print "#{number }"
+end
+
+
